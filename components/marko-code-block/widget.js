ae68421a61e1b08a2de31257266c230c3cec5930
Michael Rawlings <ml.rawlings@gmail.com>
2017-01-15 20:32:46 -0800

    reset the header after adjusting page position when switching syntax

--- components/marko-code-block/widget.js
+++ components/marko-code-block/widget.js
@@ -1,7 +1,12 @@
+var getWidgetForEl = require('marko/widgets').getWidgetForEl;
+
 module.exports = {
     changeSyntax: function() {
+        var header = getWidgetForEl(document.querySelector('.site-header'));
         var beforeScroll = document.body.scrollTop;
         var beforePosition = this.el.offsetTop;
+
+        header.save();
 
         if(localStorage.syntax === 'concise') {
             localStorage.syntax = 'html';
@@ -13,6 +18,9 @@ module.exports = {
 
         var afterPosition = this.el.offsetTop;
         var afterScroll = beforeScroll - beforePosition + afterPosition;
+
         document.body.scrollTop = afterScroll;
+
+        header.reset();
     }
 }
--- components/site-header/widget.js
+++ components/site-header/widget.js
@@ -1,9 +1,30 @@
 var Headspace = require('headspace');
+var classNames = {
+    base: 'headspace',
+    fixed: 'headspace--fixed',
+    hidden: 'headspace--hidden'
+};
 
 module.exports = {
     onMount() {
         this.headspace = new Headspace(this.el, {
-            showAtBottom: false
+            showAtBottom: false,
+            classNames: classNames
+        });
+    },
+    save() {
+        this.isHidden = this.el.classList.contains(classNames.hidden);
+    },
+    reset() {
+        var header = this;
+        header.headspace.debounce(function() {
+            setTimeout(function() {
+                if(header.isHidden) {
+                    header.el.classList.add(classNames.hidden);
+                } else {
+                    header.el.classList.remove(classNames.hidden);
+                }
+            });
         });
     }
 }
