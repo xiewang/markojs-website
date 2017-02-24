0e054783eb42ff978e4e0a893cb3b7bf51a32f15
Michael Rawlings <ml.rawlings@gmail.com>
2017-02-23 17:19:10 -0800

    add scrollspy and version switcher

--- routes/docs/components/doc-sidebar/component-browser.js
+++ routes/docs/components/doc-sidebar/component-browser.js
@@ -1,9 +1,64 @@
 var getComponentForEl = require('marko/components').getComponentForEl;
 var forEach = [].forEach;
+var filter = [].filter;
+var slice = [].slice;
 
 module.exports = {
     onMount() {
+        this.preventOverscroll();
+        this.listenForHeaderChanges();
+        this.initScrollSpy();
+    },
+
+    initScrollSpy() {
+        var headersSelector = [1,2,3,4,5,6].map(n => '.doc-content h'+n).join(',');
+        var headers = slice.call(document.querySelectorAll(headersSelector));
+        var waiting = false;
+
+        this.subscribeTo(window).on('scroll', () => {
+            if (!waiting) {
+                waiting = true;
+                setTimeout(() => {
+                    var threshold = window.innerHeight / 3;
+                    var closestHeader;
+                    var closestTop;
+
+                    headers.map(header => {
+                        var top = header.getBoundingClientRect().top;
+                        if (closestTop == null || (top < threshold && Math.abs(top) < Math.abs(closestTop))) {
+                            closestTop = top;
+                            closestHeader = header;
+                        }
+                    });
+
+                    var anchor = closestHeader.id;
+                    var anchorEl = this.el.querySelector('a[href="#'+anchor+'"]') || this.el.querySelector('a.selected');
+                    var targetAnchor = anchorEl;
+                    var childList = targetAnchor.nextSibling;
+
+                    if (childList) {
+                        forEach.call(childList.querySelectorAll('a[href^=\\#]'), (a) => a.classList.remove('selected'));
+                    }
+
+                    while (targetAnchor) {
+                        var parentList = targetAnchor.closest('ul');
+                        var siblings = parentList && filter.call(parentList.querySelectorAll(':scope > li > a[href^=\\#]'), (a) => a !== targetAnchor);
+                        siblings && siblings.forEach(a => a.classList.remove('selected'));
+                        targetAnchor.classList.add('selected');
+                        targetAnchor = parentList && parentList.previousSibling;
+                    }
+
+                    this.scrollAnchorIntoView(anchorEl);
+
+                    waiting = false;
+                }, 50);
+            }
+        });
+    },
+
+    listenForHeaderChanges() {
         var header = getComponentForEl(document.querySelector('.site-header'));
+
         forEach.call(this.el.querySelectorAll('a[href^=\\#]'), (a) => {
             this.subscribeTo(a).on('click', () => {
                 header.hide();
@@ -11,30 +66,73 @@ module.exports = {
                 header.resume();
             });
         });
+
         this.subscribeTo(this.el.querySelector('a.selected')).on('click', (e) => {
             window.scrollTo(0,0);
             header.reset();
             e.preventDefault();
         });
-        this.subscribeTo(header).on('reset', () => {
-            this.el.classList.remove('no-header');
-            this.el.classList.remove('fixed');
-            setTimeout(() => this.el.classList.remove('transition'), 0);
-        });
-        this.subscribeTo(header).on('fix', () => {
-            this.el.classList.remove('no-header');
-            this.el.classList.add('fixed');
-            setTimeout(() => this.el.classList.add('transition'), 0);
-        });
-        this.subscribeTo(header).on('hide', () => {
-            this.el.classList.add('no-header');
-            this.el.classList.add('fixed');
-            setTimeout(() => this.el.classList.add('transition'), 0);
-        });
+
+        this.subscribeTo(header)
+            .on('reset', () => {
+                this.el.classList.remove('no-header');
+                this.el.classList.remove('fixed');
+                setTimeout(() => this.el.classList.remove('transition'), 0);
+            })
+            .on('fix', () => {
+                this.el.classList.remove('no-header');
+                this.el.classList.add('fixed');
+                setTimeout(() => this.el.classList.add('transition'), 0);
+            })
+            .on('hide', () => {
+                this.el.classList.add('no-header');
+                this.el.classList.add('fixed');
+                setTimeout(() => this.el.classList.add('transition'), 0);
+            });
 
         if (window.pageYOffset > header.el.offsetHeight) {
             this.el.classList.add('no-header');
             this.el.classList.add('fixed');
+        }
+    },
+
+    preventOverscroll() {
+        var sidebar = this.getEl('sidebar');
+        this.subscribeTo(document.body).on('mousewheel', (e) => {
+            if (sidebar.scrollTop === 0 && e.deltaY < 0 || e.deltaY > 0 && sidebar.scrollTop === sidebar.scrollHeight - sidebar.offsetHeight) {
+                if (e.target === sidebar || sidebar.contains(e.target)) {
+                    e.preventDefault();
+                }
+            }
+        });
+    },
+
+    scrollAnchorIntoView(anchorEl) {
+        var sidebar = this.getEl('sidebar');
+        var anchorTop = anchorEl.offsetTop;
+        var sidebarScrollTop = sidebar.scrollTop;
+        var sidebarHeight = sidebar.offsetHeight;
+        var sidebarScrollBottom = sidebarScrollTop + sidebarHeight;
+
+        var targetList = anchorEl.closest('li');
+        var parentList;
+
+        while (true) {
+            parentList = targetList.parentNode.closest('ul');
+            if (parentList && parentList.offsetHeight < sidebarHeight) {
+                targetList = parentList;
+            } else {
+                break;
+            }
+        }
+
+        var targetTop = targetList.offsetTop;
+        var targetHeight = targetList.offsetHeight;
+        var targetBottom = targetTop + targetHeight;
+        var targetIsFullyVisible = targetTop > sidebarScrollTop && targetBottom < sidebarScrollBottom;
+
+        if (!targetIsFullyVisible) {
+            sidebar.scrollTop = targetTop + targetHeight/2 - sidebarHeight/2;
         }
     }
 }
