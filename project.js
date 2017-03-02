fee57b7fdeb996276c46b50c75def04d18cf613c
Patrick Steele-Idem <pnidem@gmail.com>
2017-03-02 13:42:50 -0700

    Added babel transpiling

--- project.js
+++ project.js
@@ -1,7 +1,5 @@
 var isProduction = process.env.NODE_ENV === 'production';
 
-
-
 function getBabiliConfig() {
     if (isProduction) {
         return {
@@ -18,6 +16,19 @@ function getBabiliConfig() {
     }
 }
 
+var requireTransforms = [];
+
+if (isProduction) {
+    requireTransforms.push({
+        transform: 'lasso-babel-transform',
+        config: {
+            extensions: ['.js', '.es6', '.marko'], // Enabled file extensions. Default: ['.js', '.es6']
+            babelOptions: {
+                presets: [require('./babel-preset')]
+            }
+        }
+    });
+}
 
 module.exports = require('marko-starter').projectConfig({
     routePathPrefix: '/',
@@ -28,7 +39,8 @@ module.exports = require('marko-starter').projectConfig({
             builtins: {
                 'fs': require.resolve('./browser-shims/fs'),
                 'module': require.resolve('./browser-shims/module')
-            }
+            },
+            transforms: requireTransforms
         },
         minifyJS: false,
         plugins: [
