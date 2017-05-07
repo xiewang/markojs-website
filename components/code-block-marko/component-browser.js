6530a65da68d8990583635868c1582527e9c781d
xiewang <xiewang0501@126.com>
2017-05-07 22:08:29 +0800

    path

--- components/code-block-marko/component-browser.js
+++ components/code-block-marko/component-browser.js
@@ -1,17 +1,24 @@
 "use strict";
 
 const getComponentForEl = require('marko/components').getComponentForEl;
-const localStorageUtil = require('~/util/localStorage.js');
+function getMarkoWebsiteKey(key) {
+    return `markojs-website:${key}`;
+}
+const localStorageUtil = {
+    get: (key) => localStorage.getItem(getMarkoWebsiteKey(key)),
+    set: (key, value) => localStorage.setItem(getMarkoWebsiteKey(key), value),
+    getMarkoWebsiteKey: getMarkoWebsiteKey
+};
 
 module.exports = {
-    changeSyntax: function() {
+    changeSyntax: function () {
         var header = getComponentForEl(document.querySelector('.site-header'));
         var beforeScroll = document.body.scrollTop;
         var beforePosition = this.el.offsetTop;
 
         header.pause();
 
-        if(localStorageUtil.get('syntax') === 'concise') {
+        if (localStorageUtil.get('syntax') === 'concise') {
             localStorageUtil.set('syntax', 'html');
             document.body.classList.remove('concise');
         } else {
